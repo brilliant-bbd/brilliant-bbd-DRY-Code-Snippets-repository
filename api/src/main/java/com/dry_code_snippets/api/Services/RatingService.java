154d112cef4b8d8423ab442d0501412622882fa0
LekalaKholofelo <kholofelo.lekala@bbd.co.za>
2025-03-19 09:42:46 +0200

    add rating

--- api/src/main/java/com/dry_code_snippets/api/Services/RatingService.java
+++ api/src/main/java/com/dry_code_snippets/api/Services/RatingService.java
@@ -7,25 +7,61 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
 import com.dry_code_snippets.api.Models.Rating;
+import com.dry_code_snippets.api.Models.Snippet;
 import com.dry_code_snippets.api.Repositories.RatingRepository;
+import com.dry_code_snippets.api.Repositories.SnippetRepository;
+
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.time.LocalDateTime;
 
 @Service
 public class RatingService {
 
     private final RatingRepository ratingRepository;
 
+    private final SnippetRepository snippetRepository;
+
     private final UserRepository userRepository;
 
     @Autowired
-    public RatingService(RatingRepository ratingRepository, UserRepository userRepository) {
+    public RatingService(RatingRepository ratingRepository, SnippetRepository snippetRepository, UserRepository userRepository) {
         this.ratingRepository = ratingRepository;
+        this.snippetRepository = snippetRepository;
         this.userRepository = userRepository;
     }
 
     public Rating addRating(Long snippetId, Integer ratingNum) {
         User user = userRepository.findByUserGuid(Shared.getClaim()).orElseThrow();
-        Rating rating= new Rating(snippetId,user.getUserId(),ratingNum);
+
+        Snippet snippet = snippetRepository.findById(snippetId)
+                .orElseThrow(() -> new NoSuchElementException("Snippet not found for ID: " + snippetId));
+
+        if (snippet.getUserId().equals(user.getUserId())) {
+            throw new IllegalArgumentException("Users cannot rate their own snippets.");
+        }
+
+        if (snippet.isDeleted()) {
+            throw new IllegalArgumentException("Snippet is deleted for ID: " + snippetId);
+        }
+
+        List<Rating> allRatings = ratingRepository.findBySnippetId(snippetId);
+
+        Rating rating = null;
+        for (Rating rate : allRatings) {
+            if (rate.getUserId() == user.getUserId()) {
+                rating = rate;
+                break;
+            }
+        }
+
+        if (rating == null) {
+            rating = new Rating(snippetId,user.getUserId(),ratingNum);
+        } else {
+            rating.setRating(ratingNum);
+            rating.setCreatedAt(LocalDateTime.now());
+        }
+
         return ratingRepository.save(rating);
     }
 
